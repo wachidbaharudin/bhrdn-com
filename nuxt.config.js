27de3c8d52bfc33cd69391f8c93dce7f45e92b8f
Wachid Baharudin <wabahproject@gmail.com>
2020-09-13 00:55:55 +0700

    update config and add link

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,4 @@
 export default {
-  buildModules: ['@nuxtjs/tailwindcss']
+  buildModules: ['@nuxtjs/tailwindcss'],
+  target: 'static'
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="container mx-auto min-h-screen flex">
-    <div class="self-center flex flex-auto justify-center"> 
-        <img
-          class="rounded-full w-40 h-40 p-2 flex-shrink-0"
-          src="https://tailwindcss.com/img/card-top.jpg"
-          alt="Sunset in the mountains"
-        />
-        <div class="text-left px-6 self-center ">
-          <h3 class="font-grey-700 text-base">Hi, my name is</h3>
-          <h1 class="font-bold text-xl">Wachid Baharudin</h1>
-          <h3 class="font-grey-700 text-base">I am Node.js Developer</h3>
+    <div class="self-center flex flex-auto justify-center">
+      <img
+        class="rounded-full w-40 h-40 p-2 flex-shrink-0"
+        src="~assets/img/profile.jpg"
+        alt="Sunset in the mountains"
+      />
+      <div class="text-left px-6 self-center">
+        <h3 class="font-grey-700 text-base">Hi, my name is</h3>
+        <h1 class="font-bold text-xl">Wachid Baharudin</h1>
+        <h3 class="font-grey-700 text-base">I am Node.js Developer</h3>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/wachidbaharudin/"
+            class="no-underline hover:underline text-blue-500"
+            target="_blank"
+          >linkedin</a>
+          <a
+            href="https://github.com/wachidbaharudin"
+            class="no-underline hover:underline text-blue-500 ml-2"
+            target="_blank"
+          >github</a>
         </div>
+      </div>
     </div>
   </div>
 </template>
