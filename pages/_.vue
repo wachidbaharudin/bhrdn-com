0ec97e8b0b09a11a0b4cf06e589ab0d0662487e9
Wachid Baharudin <wabahproject@gmail.com>
2020-09-12 23:59:10 +0700

    Initial commit

---
+++ pages/_.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container mx-auto min-h-screen flex">
+    <div class="self-center flex flex-auto justify-center">
+      <div class="text-center">
+        <h1 class="font-bold text-xl">404 | there is nothing here</h1>
+        <NuxtLink to="/" class="no-underline hover:underline text-blue-500 text-lg">Back Home</NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container mx-auto min-h-screen flex">
+    <div class="self-center flex flex-auto justify-center"> 
+        <img
+          class="rounded-full w-40 h-40 p-2 flex-shrink-0"
+          src="https://tailwindcss.com/img/card-top.jpg"
+          alt="Sunset in the mountains"
+        />
+        <div class="text-left px-6 self-center ">
+          <h3 class="font-grey-700 text-base">Hi, my name is</h3>
+          <h1 class="font-bold text-xl">Wachid Baharudin</h1>
+          <h3 class="font-grey-700 text-base">I am Node.js Developer</h3>
+        </div>
+    </div>
+  </div>
+</template>
